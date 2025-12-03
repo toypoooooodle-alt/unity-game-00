28b1f3918f64a3a42687bea3476091741064811d
パコソン\kazum <toypoooooodle@gmail.com>
2025-12-03 19:08:37 +0900

    プレイヤー移動

--- Assets/Project/Scripts/Sex player contol.cs
+++ Assets/Project/Scripts/Sex player contol.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class Sexplayercontol : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 50f;
+    [SerializeField] private float nowSpeed = 0f;
+    [SerializeField] private float maxSpeed = 20f;
+    public float HalfMoveLimit = 8f;
+
+    private Transform transform;
+
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-    Debug.Log("aaa");
+        // スピード変更
+        if (nowSpeed > 0)
+        {
+            nowSpeed -= 10 * Time.deltaTime;
+        }
+        else if (nowSpeed < 0)
+        {
+            nowSpeed += 10 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            nowSpeed -= moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            nowSpeed += moveSpeed * Time.deltaTime;
+        }
+
+        if (nowSpeed >= maxSpeed)
+        {
+            nowSpeed = maxSpeed;
+        }
+
+        if (nowSpeed <= -maxSpeed)
+        {
+            nowSpeed = -maxSpeed;
+        }
+
+        // 移動
+        transform.position += nowSpeed * Time.deltaTime * Vector3.right;
+
+        if (transform.position.x >= HalfMoveLimit)
+        {
+            transform.position = new Vector3(HalfMoveLimit, transform.position.y, transform.position.z);
+            nowSpeed = 0f;
+        }
+
+        if (transform.position.x <= -HalfMoveLimit)
+        {
+            transform.position = new Vector3(-HalfMoveLimit, transform.position.y, transform.position.z);
+            nowSpeed = 0f;
+        }
     }
 }
